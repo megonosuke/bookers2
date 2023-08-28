6670de41cfe0487f72634b1a4e285aa952104ec1
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-08-28 23:42:36 +0000

    [Rest]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.new
+    @books = Book.all
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
