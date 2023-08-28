f77ffbebb7075bc1fb18bb211d942308c322fe20
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-08-28 18:08:58 +0000

    [Rest]

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  
+  root to: 'homes#top'
   devise_for :users
+  
+  resources :books, only: [:edit, :index, :show]
+  resources :users, only: [:edit, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
