9d9c148d8675a7a89ab6bc80b23d8c7802fcb841
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-08-29 08:15:58 +0000

    [Rest]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(:id)
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
