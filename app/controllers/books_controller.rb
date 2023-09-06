7d7a1a99e2dcb81bfe658778db48399e763af065
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-09-06 05:43:56 +0000

    [Rest]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,5 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
-    @books = Book.all
-  end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
   end
 
   def show
@@ -15,11 +7,4 @@ class BooksController < ApplicationController
 
   def edit
   end
-
-  private
-
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @book = Book.new
-    @books = Book.all
   end
 
   def index
-    @book = Book.new
-    @books = Book.all
   end
 
   def edit
-  end
-  
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :profile_image)
   end
 end
