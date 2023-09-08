1e5bc6b018c52985ca6095fb197c72e0c3f2ecd3
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-09-08 16:17:10 +0000

    [Rest]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
       @books = Book.all
@@ -33,6 +33,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book)
     else
       @books = Book.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
    @user = User.find(params[:id])
    @books = @user.books
@@ -8,12 +10,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(current_user)
     else
       @books = Book.all
@@ -25,6 +28,13 @@ class UsersController < ApplicationController
 
   def user_params
       params.require(:user).permit(:name, :prpfile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+        redirect_to user_path(current_user)
+    end
   end
 
 end
